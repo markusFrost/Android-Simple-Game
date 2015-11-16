f633c667eb9df6beb8daedfffda482dc69a1b770
markusFrost <andreywystawkin@yandex.ru>
2015-11-16 19:33:49 +0300

    Initial commit

---
+++ app/src/androidTest/java/rgb_circle_name/ru/whitemonkteam/funny_time/rgbcircle/ApplicationTest.java
@@ -0,0 +1,13 @@
+package rgb_circle_name.ru.whitemonkteam.funny_time.rgbcircle;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
